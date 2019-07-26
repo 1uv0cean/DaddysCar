d01adff0db5bdc32f76ae1c6a91ae4ca8c312bb6
김건우 <skawk159357@naver.com>
2019-07-26 22:42:25 +0900

    kakaologin

--- app/src/main/java/com/mojuk/daddyscar/LoginActivity.java
+++ app/src/main/java/com/mojuk/daddyscar/LoginActivity.java
@@ -4,13 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.kakao.auth.AuthType;
+import com.kakao.auth.Session;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView txt_find_info;
+
+    private Button kakao_btn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,7 +27,26 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_login);
 
         txt_find_info = findViewById(R.id.txt_find_info);
+
+        kakao_btn = (Button) findViewById(R.id.kakao_btn);
+
+        kakao_btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View view) {
+
+                Session session = Session.getCurrentSession();
+
+                session.addCallback(new SessionCallback());
+
+                session.open(AuthType.KAKAO_LOGIN_ALL, LoginActivity.this);
+
+            }
+
+        });
     }
+
 
     @Override
     public void onClick(View view) {
---
+++ app/src/main/java/com/mojuk/daddyscar/RegisterActivity.java
@@ -0,0 +1,7 @@
+package com.mojuk.daddyscar;
+
+import com.android.volley.toolbox.StringRequest;
+
+/**
+ * Created by GunWoo.Kim on 2019-07-26.
+ */
